96423c194124f880d615aa59440460cddb4d9831
Fernando Lopez <lopezfer@globalhitss.com>
2021-07-28 22:49:00 -0300

    fixs payments

--- payment-subscription/contracts/Payment.sol
+++ payment-subscription/contracts/Payment.sol
@@ -6,4 +6,22 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract Payment {
     uint public nextPlanId;
+
+    // estructura Plan
+    struct Plan {
+        address merchant; // comerciante
+        address token;
+        uint ammount; //cantidad
+        uint frequency;
+    }
+
+    // estructura de la subscripción
+    struct Subscription {
+        address subscriber;
+        uint start;
+        uint nextPayment; 
+    }
+
+    mapping(uint => Plan) public plans;
+    mapping(address => mapping(uint => Subscription)) public Subscriptions;
 }
