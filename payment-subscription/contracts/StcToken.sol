8b98b5313e91164e22c2e865c4527fa320d6e726
Fernando Lopez <lopezfer@globalhitss.com>
2021-07-28 22:15:25 -0300

    add mock token and contract payment

---
+++ payment-subscription/contracts/StcToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract StcToken is ERC20 {
+    constructor() ERC20('StcToken', 'STC') {
+        _mint(msg.sender, 10000);
+    }
+}
