865c8c44bd18771f1be85f36c34ad8a7dd58e3e0
Fernando Lopez <lopezfer@globalhitss.com>
2021-08-01 16:51:55 -0300

    add function create plan

--- payment-subscription/contracts/Payment.sol
+++ payment-subscription/contracts/Payment.sol
@@ -24,4 +24,37 @@ contract Payment {
 
     mapping(uint => Plan) public plans;
     mapping(address => mapping(uint => Subscription)) public Subscriptions;
+
+    event PlanCreado(
+        address merchant,
+        uint planId,
+        uint date
+    );
+
+    event SubscriptionCancelled(
+        address subscriber,
+        uint planId,
+        uint date
+    );
+
+    event PaymentSent(
+        address from,
+        address to,
+        uint amount,
+        uint planId,
+        uint date
+    );
+
+    function crearPlan(address token, uint amount, uint frequency) external {
+        require(token != address(0), 'addres cannot be nul address');
+        require(amount > 0, 'amount needs to be > 0');
+        require(frequency > 0, 'frequency needs to be > 0');
+        plans[nextPlanId] = Plan(
+            msg.sender,
+            token,
+            amount,
+            frequency
+        );
+        nextPlanId++;
+    }
 }
